f9d396ab78dc1c27560d52996f1e92f8b38aa82a
Diego <dval@salesforce.com>
2017-12-23 16:53:31 -0700

    Add best

--- examples/simple_benchmark/src/main.js
+++ examples/simple_benchmark/src/main.js
@@ -2,5 +2,5 @@ import { createElement } from "engine";
 import App from "simple-benchmark";
 
 const container = document.getElementById('main');
-const element = createElement('todo-app', { is: App });
+const element = createElement('simple-benchmark', { is: App });
 container.appendChild(element);
