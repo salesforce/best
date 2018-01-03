ec516996b0c44d209c8eb70d8f2d49687008c141
Diego <dval@salesforce.com>
2018-01-03 12:40:32 -0800

    Revert remote runner in the example

--- examples/simple_benchmark/best.config.js
+++ examples/simple_benchmark/best.config.js
@@ -6,11 +6,12 @@ module.exports = {
         }
     },
     "benchmarkOnClient": false,
-    "benchmarkRunner": "@best/runner-remote",
-    "benchmarkRunnerConfig": {
-        "host": "http://localhost:5000",
-        "options": { path: '/best' },
-        "remoteRunner": "@best/runner-headless"
-    },
+    "benchmarkRunner": "@best/runner-headless",
 
+    // This is for running @best/runner-remote
+    // "benchmarkRunnerConfig": {
+    //     "host": "http://localhost:5000",
+    //     "options": { path: '/best' },
+    //     "remoteRunner": "@best/runner-headless"
+    // },
 };
