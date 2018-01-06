a32689db9fefa72d4c85f99eaf9bc849a8eb9022
Diego <dval@salesforce.com>
2018-01-05 19:01:49 -0800

    Integrations

---
+++ packages/best-github-integration/src/git-app.js
@@ -0,0 +1,50 @@
+/* eslint camelcase: 0 */
+
+import jwt from "jsonwebtoken";
+import GitHubApi from "github";
+
+const APP_ID = process.env.GIT_APP_ID;
+const APP_CERT = process.env.GIT_APP_CERT;
+
+function generateJwt(id, cert) {
+    const payload = {
+        iat: Math.floor(new Date() / 1000),       // Issued at time
+        exp: Math.floor(new Date() / 1000) + 60,  // JWT expiration time
+        iss: id                                   // Integration's GitHub id
+    };
+
+    // Sign with RSA SHA256
+    return jwt.sign(payload, cert, { algorithm: 'RS256' });
+}
+
+class GitHubApp {
+    constructor(id, cert, opts = {}) {
+        this.id = id;
+        this.cert = cert;
+        this.opts = opts;
+    }
+
+    async authAsApp() {
+        const { id, cert } = this;
+        const github = new GitHubApi(this.opts);
+        github.authenticate({ type: 'integration', token: generateJwt(id, cert) });
+        return github;
+    }
+
+    async authAsInstallation(installationId) {
+        const token = await this.createToken(installationId);
+        const github = new GitHubApi(this.opts);
+        github.authenticate({ type: 'token', token });
+        return github;
+    }
+
+    async createToken(installation_id) {
+        const github = await this.authAsApp();
+        const response = await github.apps.createInstallationToken({ installation_id });
+        return response.data.token;
+    }
+}
+
+export function createGithubApp(id = APP_ID, cert = APP_CERT) {
+    return new GitHubApp(id, cert);
+}
--- packages/best-github-integration/src/index.js
+++ packages/best-github-integration/src/index.js
@@ -1,22 +1,22 @@
-import { preRunMessager } from "@best/messager";
+import { createGithubApp } from "./git-app";
 import { isCI } from "@best/utils";
 
-const APP_ID = process.env.GIT_APP_ID;
-const APP_CERT = process.env.GIT_APP_CERT;
 const TARGET_COMMIT = process.env.TARGET_COMMIT;
 const BASE_COMMIT = process.env.BASE_COMMIT;
 
-function checkIntegrationEnviroment() {
-    return isCI && APP_ID && APP_CERT;
-}
-
 export async function pushBenchmarkComparison(compareStats) {
-    console.log('APP_ID >> ', APP_ID);
-    console.log('APP_CERT >> ', APP_CERT);
     console.log('BASE >> ', BASE_COMMIT);
     console.log('TARGET >> ', TARGET_COMMIT);
 
-    if (!checkIntegrationEnviroment()) {
+    if (!isCI()) {
         //throw new Error('GitIntegration: Some of the environment configuration is not properly setup');
+        console.log('SHould not log this in CI');
     }
+
+    const APP = createGithubApp();
+    const gitAppAuth = await APP.authAsApp();
+    const installations = gitAppAuth.apps.getInstallations({});
+
+    console.log('>> ', installations);
+
 }
