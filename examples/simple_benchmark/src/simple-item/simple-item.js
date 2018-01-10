84bf066fcd8661658113e41f113aa0af49ce50de
Diego <dval@salesforce.com>
2018-01-10 14:44:08 -0800

    Increase iterations

--- examples/simple_benchmark/src/simple-item/simple-item.js
+++ examples/simple_benchmark/src/simple-item/simple-item.js
@@ -1,6 +1,6 @@
 import { Element } from "engine";
 
-const ITEMS = Array.apply(null, Array(4000)).map((k, i) => i);
+const ITEMS = Array.apply(null, Array(5000)).map((k, i) => i);
 
 export default class SimpleBench extends Element {
     @track test = 'benchnark';
