dd1834104eb749645b5c3c299276e4c8f6f27bc6
Diego <dval@salesforce.com>
2018-03-06 18:53:27 -0800

    Add compat config

---
+++ examples/simple_lwc_benchmark/best.prod.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    projectName: 'lwc-examples-prod',
+    plugins: [
+        '<rootDir>/custom-rollup-transformer/empty-example.js',
+        ['rollup-plugin-lwc-compiler', {
+            rootDir: '<rootDir>/src/',
+            mode: 'prod', // We don't really need prod here since this is for test best itself
+        }]
+    ],
+    benchmarkOnClient: false,
+    benchmarkRunner: '@best/runner-headless',
+
+    // This is for running @best/runner-remote
+    // "benchmarkRunnerConfig": {
+    //     "host": "http://localhost:5000",
+    //     "options": { path: '/best' },
+    //     "remoteRunner": "@best/runner-headless"
+    // },
+};
