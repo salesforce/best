013352118fb8e8e8544afcc4e2cd2d970a069713
Diego <dval@salesforce.com>
2018-03-18 17:55:36 -0700

    fix: Missing await

--- packages/best-cli/src/run_compare.js
+++ packages/best-cli/src/run_compare.js
@@ -59,7 +59,7 @@ export async function runCompare(globalConfig, configs, outputStream) {
             preRunMessager.print('\n Fetching benchmark results to compare... \n\n', outputStream);
             compareResults = await compareBenchmarkStats(baseCommit, compareCommit, projectNames, storageProvider);
         } else {
-            git.checkout(baseCommit);
+            await git.checkout(baseCommit);
             await runBest({ ...globalConfig, gitCommit: baseCommit }, configs, outputStream);
 
             console.log('WIP');
