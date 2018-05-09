e4110af6446ec65fae3d81c7022222eba93d32a5
Sam <seubank@salesforce.com>
2018-05-09 14:55:32 -0700

    chore: Fix "benchnark" spelling

--- examples/simple_lwc_benchmark/src/simple-item/simple-item.js
+++ examples/simple_lwc_benchmark/src/simple-item/simple-item.js
@@ -3,6 +3,6 @@ import { Element } from 'engine';
 const ITEMS = Array.apply(null, Array(1000)).map((k, i) => i);
 
 export default class SimpleBench extends Element {
-    @track test = 'benchnark';
+    @track test = 'benchmark';
     items = ITEMS;
 }
