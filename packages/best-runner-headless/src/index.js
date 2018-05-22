62fbd891ca4734de18f2c8a57801643ba2c218ca
Dinko <dbajric@salesforce.com>
2018-05-22 15:29:09 -0700

    refactor: Moved getSystemInfo to best-utils for reusability

--- packages/best-runner-headless/src/index.js
+++ packages/best-runner-headless/src/index.js
@@ -1,5 +1,5 @@
 import puppeteer from 'puppeteer';
-import { getSystemInfo } from './system-info';
+import { getSystemInfo } from '@best/utils';
 import { clearInterval } from 'timers';
 
 const BROWSER_ARGS = [
