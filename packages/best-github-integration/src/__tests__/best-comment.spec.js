2317adcd6cad58a219b7690124f56c0edbc587fa
Nolan Lawson <nlawson@salesforce.com>
2019-02-04 09:47:49 -0800

    fix: fix tests based on code review

--- packages/best-github-integration/src/__tests__/best-comment.spec.js
+++ packages/best-github-integration/src/__tests__/best-comment.spec.js
@@ -1,10 +1,10 @@
-import comparison1 from './fixtures/comparison1';
-import { generateComparisonComment } from '../../src/comment';
 import fs from 'fs';
 import path from 'path';
+import comparison1 from './fixtures/comparison1';
+import { generateComparisonComment } from '../../src/comment';
 
 describe('generateComment', () => {
-    test('fail', () => {
+    test('fixtures/comparison1.js', () => {
         const { baseCommit, targetCommit, stats } = comparison1;
         const actual = generateComparisonComment(baseCommit, targetCommit, stats);
         const expected = fs.readFileSync(path.resolve(__dirname, 'fixtures/expected1.md'), 'utf8');
