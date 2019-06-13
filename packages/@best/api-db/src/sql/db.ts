bd15d64df4d681a8a2a01fb0474dc199984ab663
Jason Silberman <jsilberman@salesforce.com>
2019-06-13 15:25:21 -0700

    fix(api-db): fix sql query to support both sqlite and postgres

--- packages/@best/api-db/src/sql/db.ts
+++ packages/@best/api-db/src/sql/db.ts
@@ -24,10 +24,10 @@ export abstract class SQLDatabase {
 
     fetchSnapshots(projectId: number, since: string): Promise<SQLQueryResult> {
         if (since) {
-            return this.query(`SELECT * FROM snapshots WHERE "project_id" = $1 AND "temporary" = 'f' AND "commit_date" > $2 ORDER BY commit_date, name`, [projectId, since])
+            return this.query(`SELECT * FROM snapshots WHERE "project_id" = $1 AND "temporary" = '0' AND "commit_date" > $2 ORDER BY commit_date, name`, [projectId, since])
         }
 
-        return this.query(`SELECT * FROM snapshots WHERE "project_id" = $1 AND "temporary" = 'f' ORDER BY commit_date, name`, [projectId])
+        return this.query(`SELECT * FROM snapshots WHERE "project_id" = $1 AND "temporary" = '0' ORDER BY commit_date, name`, [projectId])
     }
 
     fetchProject(name: string): Promise<SQLQueryResult> {
