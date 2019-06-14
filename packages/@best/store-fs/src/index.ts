c9e2014b624d746aa622c8974acb5e9e597da77a
Jason Silberman <jsilberman@salesforce.com>
2019-06-14 09:26:38 -0700

    wip: new github-integration
    * fix: store-fs now uses absolute paths from ROOT_DIR

--- packages/@best/store-fs/src/index.ts
+++ packages/@best/store-fs/src/index.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import globby from 'globby';
+import path from 'path';
 
 let ROOT_DIR = process.cwd();
 const IGNORE_PATHS = [
@@ -23,7 +24,7 @@ export async function getAllBenchmarkStatsPerCommit(projectName: string, commit:
     const pattern = `**/${projectName}/*.benchmark_${commit}/stats.json`;
     const results = await globby([pattern], { cwd: ROOT_DIR, ignore: IGNORE_PATHS });
     const statsResults = results.map(statsPath => {
-        const stats = JSON.parse(fs.readFileSync(statsPath, 'utf8'));
+        const stats = JSON.parse(fs.readFileSync(path.resolve(ROOT_DIR, statsPath), 'utf8'));
         return { ...stats, projectName };
     });
     return statsResults;
