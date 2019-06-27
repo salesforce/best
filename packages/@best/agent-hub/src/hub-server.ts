c161faa508e0eea090ef941b257b7a98c538f7ea
Jose David Rodriguez Velasco <jodarove@gmail.com>
2019-06-26 20:40:48 -0700

    feat: auth in hub based on jsonwebtoken

--- packages/@best/agent-hub/src/hub-server.ts
+++ packages/@best/agent-hub/src/hub-server.ts
@@ -1,4 +1,5 @@
 import socketIO, * as SocketIO from 'socket.io';
+import * as jwt from 'jsonwebtoken';
 import BenchmarkJob from "./BenchmarkJob";
 import ObservableQueue from "./utils/ObservableQueue";
 import { createAgentManager } from "./AgentManager";
@@ -6,6 +7,7 @@ import { HubApplication } from "./HubApplication";
 import { AgentConfig } from "./Agent";
 
 export interface HubConfig {
+    tokenSecret: string,
     agents: AgentConfig[],
 }
 
@@ -21,6 +23,22 @@ function createHubApplication(config: HubConfig): HubApplication {
 export function runHub(server: Express.Application, hubConfig: HubConfig) {
     const socketServer: SocketIO.Server = socketIO(server, { path: '/hub' });
     const hub: HubApplication = createHubApplication(hubConfig);
+
+    // middleware
+    socketServer.use((socket, next) => {
+        const token = socket.handshake.query.token;
+
+        // verify token
+        jwt.verify(token, hubConfig.tokenSecret, (err: Error, payload: any) => {
+            if (err) {
+                return next(new Error('authentication error: ' + err.message));
+            } else if (payload.scope !== 'client') {
+                return next(new Error('authentication error: invalid token'));
+            }
+
+            next();
+        });
+    });
 
     socketServer.on('connect', (socket) => {
         hub.handleIncomingSocketConnection(socket)
