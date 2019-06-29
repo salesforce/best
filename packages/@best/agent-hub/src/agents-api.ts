ce4ff5bdaac7084160afdcb60ef8c9295fec606d
Jose David Rodriguez Velasco <jodarove@gmail.com>
2019-06-28 17:41:45 -0700

    feat: autoregister agent with hub

--- packages/@best/agent-hub/src/agents-api.ts
+++ packages/@best/agent-hub/src/agents-api.ts
@@ -1,19 +1,19 @@
-import { Application, NextFunction, Request, Response } from "express";
+import {Application, NextFunction, Request, Response} from "express";
 import * as jwt from "jsonwebtoken";
 import {AgentManager} from "./AgentManager";
-import {Agent} from "./Agent";
+import {Agent, AgentStatus} from "./Agent";
 
 function authenticateAgentApi(tokenSecret: string, req: Request, res: Response, next: NextFunction) {
     if (req.path.startsWith('/api/v1')) {
         jwt.verify(req.headers.authorization || '', tokenSecret, (err: Error, payload: any) => {
             if (err) {
                 return res.status(403).send({
-                    success: 'fasle',
+                    success: 'false',
                     message: 'Authentication error: ' + err.message
                 });
             } else if (payload.scope !== 'agent') {
                 return res.status(403).send({
-                    success: 'fasle',
+                    success: 'false',
                     message: 'Authentication error: Invalid token'
                 });
             }
@@ -39,9 +39,24 @@ function addAgentToHub(agentManager: AgentManager, req: Request, res: Response) 
 
     agentManager.addAgent(agent);
 
+    console.log('Added agent with host and spec: ', agentConfig.host, JSON.stringify(agentConfig.spec));
+
     return res.status(201).send({
         success: 'true',
         message: 'Agent subscribed successfully'
+    });
+}
+
+function pingByAgent(agentManager: AgentManager, req: Request, res: Response) {
+    const agent: Agent | null = agentManager.getAgent(req.query.agent);
+
+    if (agent && agent.status === AgentStatus.Offline) {
+        agent.status = AgentStatus.Idle;
+    }
+
+    return res.status(200).send({
+        success: 'true',
+        agentStatus: agent === null ? 'disconnected' : 'connected'
     });
 }
 
@@ -49,4 +64,5 @@ export function configureAgentsApi(app: Application, agentManager: AgentManager,
     app.use(authenticateAgentApi.bind(null, tokenSecret));
 
     app.post('/api/v1/agents', addAgentToHub.bind(null, agentManager));
+    app.get('/api/v1/agent-ping', pingByAgent.bind(null, agentManager));
 }
---
+++ packages/@best/agent/README.md
@@ -0,0 +1,44 @@
+# Best Hub
+
+A Best agent is a machine used to run BEST tests in an isolated environment, ensuring nothing changes. The agent usually runs with an specific best runner (ex: `@best/runner-headless` or `@best/runner-webdriver`) with the specific config
+
+## Starting the agent
+
+## Registration with a hub
+
+When the best agent starts, if `env.HUB_CONFIG` is set, it will try to auto register with the specified hub.
+
+`env.HUB_CONFIG` is a JSON string representing an object in the following form:
+
+```js
+const hubConfig = {
+    hub: { // Hub Connection settings.
+        host: "http://localhost:6000",
+        authToken: 'agent token used for authentication with the hub',
+        pingTimeout: 180000, // Optional: 180000ms (3 minutes) is the default ping timout.
+    },
+    agentConfig: {
+         spec: { // Only benchmarks with this spec can run in this agent.
+             browser: "chrome",
+             version: "76"
+         },
+         host: "http://localhost:5000", // Required. Url used by the hub to connect to this agent.
+         options: { path: "/best" }, // Connection Options
+         remoteRunner: "@best/runner-headless", // Required. The runner which this agent will use when running the job. 
+         remoteRunnerConfig: {}, // Required (may be an empty object). The Runner config for this agent.
+     },
+}
+
+// set env.HUB_CONFIG=JSON.stringify(hubConfig)
+```
+
+### Communication registration process between the agent and the hub
+
+When the agent starts, with a hub config, it will start the communication with the hub in the following protocol:
+
+1. Every `hubConfig.pingTimeout` the agent will do a ping request to the hub, and the hub can reply with
+    1. registered=false. In this case the hub will make a request to register with the hub (2)
+    2. registered=true. Nothing to do, the hub already has this agent registered.
+    3. authentication failed. Any further communication is suspended since the connection token is invalid.
+    
+2. Register agent with hub: the hub will return a status.code 201 in case of success, a different status code on failure to register the agent.
