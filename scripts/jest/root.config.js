03e271b7ebbab88aa19a554c2d8f6ad6b45efe01
Jason Silberman <jsilberman@salesforce.com>
2019-07-01 17:25:31 -0700

    wip: remove config

--- scripts/jest/root.config.js
+++ scripts/jest/root.config.js
@@ -27,11 +27,11 @@ module.exports = {
         '<rootDir>/packages/@best/utils',
         '<rootDir>/packages/@best/regex-util',
         // '<rootDir>/packages/@best/builder',
-        '<rootDir>/packages/@best/config',
+        // '<rootDir>/packages/@best/config',
         '<rootDir>/packages/@best/console-stream',
         '<rootDir>/packages/@best/github-integration',
         '<rootDir>/packages/@best/frontend',
         '<rootDir>/packages/@best/runner-headless',
-        '<rootDir>/packages/@best/cli',
+        // '<rootDir>/packages/@best/cli',
     ]
 };
