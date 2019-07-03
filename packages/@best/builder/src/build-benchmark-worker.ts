9fb34a408dddfba6f0b71ddeb20d3c6b562b8ee7
Diego Ferreiro Val <dval@salesforce.com>
2019-07-03 11:00:27 -0700

    fix: types

--- packages/@best/builder/src/build-benchmark-worker.ts
+++ packages/@best/builder/src/build-benchmark-worker.ts
@@ -1,26 +1,31 @@
 import { buildBenchmark } from "./build-benchmark";
 
+const send = process.send && process.send.bind(process);
+
+if (!send) {
+    throw new Error('This module must be used as a forked child');
+}
+
 const messagerAdapter = {
     onBenchmarkBuildStart(benchmarkPath: string) {
-        process.send!({
+        send({
             type: 'messager.onBenchmarkBuildStart',
             benchmarkPath
         });
     },
     log(message: string) {
-        process.send!({
+        send({
             type: 'messager.log',
             message,
         });
     },
     onBenchmarkBuildEnd(benchmarkPath: string) {
-        process.send!({
+        send({
             type: 'messager.onBenchmarkBuildEnd',
             benchmarkPath
         });
     }
 };
-
 
 module.exports = async function (input: any, callback: Function) {
     const result = await buildBenchmark(
