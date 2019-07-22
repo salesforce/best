c3719b5e0f4e3964fac766cddb00970ecb9d8395
Jason Silberman <jsilberman@salesforce.com>
2019-07-22 16:00:47 -0700

    fix: remove reference to decorators in babel

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,6 @@ module.exports = {
         "@babel/preset-typescript"
     ],
     plugins: [
-        ["@babel/plugin-proposal-decorators", { legacy: true }],
         "@babel/plugin-proposal-class-properties",
         "@babel/transform-modules-commonjs",
     ]
