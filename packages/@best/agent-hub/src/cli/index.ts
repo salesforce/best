77824822b17accd986537cc70b56504a49f87ab9
Jason Silberman <jsilberman@salesforce.com>
2019-07-23 15:02:56 -0700

    feat: socket server path is now always /best

--- packages/@best/agent-hub/src/cli/index.ts
+++ packages/@best/agent-hub/src/cli/index.ts
@@ -1,6 +1,7 @@
 import { readFileSync } from 'fs';
 import express from 'express';
 import { HubConfig, runHub } from '../hub-server';
+import { serveFrontend } from '@best/agent-frontend';
 
 const PORT = process.env.PORT || 5000;
 const SSL_PFX_FILE = process.env.SSL_PFX_FILE;
@@ -23,6 +24,7 @@ function getDefaultConfig(tokenSecret: string, configAsJSON?: string): HubConfig
 
 export function run(config?: HubConfig) {
     const app = express();
+    serveFrontend(app);
     
     const enableHttps = SSL_PFX_FILE && SSL_PFX_PASSPHRASE;
     const http = require(enableHttps ? 'https' : 'http');
