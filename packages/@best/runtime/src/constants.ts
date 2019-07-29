16a30000321e1b47cb6f8c0f13dee91ca3fd81c8
Jason Silberman <jasonsilberman@users.noreply.github.com>
2019-07-29 12:40:12 -0700

    chore: remove beforeEach and afterEach hooks (#189)
    This closes #142.

--- packages/@best/runtime/src/constants.ts
+++ packages/@best/runtime/src/constants.ts
@@ -1,8 +1,6 @@
 const BEFORE_ALL = 'beforeAll';
-const BEFORE_EACH = 'beforeEach';
 const BEFORE = 'before';
 const AFTER_ALL = 'afterAll';
-const AFTER_EACH = 'afterEach';
 const AFTER = 'after';
 
 const MODE_ONLY = 'only';
@@ -11,10 +9,8 @@ const MODE_SKIP = 'skip';
 export const MODES = { ONLY: MODE_ONLY, SKIP: MODE_SKIP };
 export const HOOKS = {
     BEFORE_ALL,
-    BEFORE_EACH,
     BEFORE,
     AFTER_ALL,
-    AFTER_EACH,
     AFTER,
 };
 export const RUN_BENCHMARK = 'run_benchmark';
