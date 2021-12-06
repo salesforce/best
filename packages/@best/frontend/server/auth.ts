d6f48574429dbc13c1a6f7a0471b5fda2563ba10
Dinko Bajric <dbajric@salesforce.com>
2021-12-06 11:26:32 -0800

    chore: improve response message for auth-rejected requests

--- packages/@best/frontend/server/auth.ts
+++ packages/@best/frontend/server/auth.ts
@@ -27,17 +27,29 @@ function isRevoked(token: string): boolean {
  */
 export function authorizeRequest(req: Request, res: Response, next: NextFunction): void {
     const { authorization: authHeader } = req.headers;
-    const token = authHeader && authHeader.split(' ')[1]
 
     // Send unauthorized response if token is not provided in the request
-    if (token == null) {
-        res.sendStatus(401);
+    if (authHeader == null) {
+        res.status(401).send("Unauthorized: You must provide your token in authorization header.");
         return;
     }
 
+    const authHeaderParts = authHeader.split(' ');
+    if (authHeaderParts.length !== 2 || authHeaderParts[0] !== "Bearer") {
+        res.status(401).send("Unauthorized: Unrecognized authorization header format. Accepted format: Bearer <token>");
+        return;
+    }
+
+    // Second part of the auth header value is the actual token.
+    // Example header: "Authorization: Bearer this_is_my_token"
+    const token = authHeaderParts[1];
+
+    // Use last 6 chars to identify the token
+    const partialToken = token.slice(-6);
+
     // Block request if token has been revoked
     if (isRevoked(token)) {
-        res.sendStatus(403);
+        res.status(403).send("Forbidden: Your token has been revoked.");
         return;
     }
 
@@ -45,8 +57,20 @@ export function authorizeRequest(req: Request, res: Response, next: NextFunction
         // Block request if token is invalid or expired
         if (err) {
             // eslint-disable-next-line no-console
-            console.error(err);
-            res.sendStatus(403);
+            console.error(`Error while authorizing request using token ending in '${partialToken}'. ${err}`);
+
+            // Always send with status 403 Forbidden
+            res.status(403);
+
+            // Respond back with specific reasons for denial if reason is known. 
+            if (err instanceof jwt.TokenExpiredError) {
+                res.send(`Token ending in '${partialToken}' expired on ${err.expiredAt}`);
+            } else if (err instanceof jwt.JsonWebTokenError) {
+                res.send(`Token ending in '${partialToken}' cannot be parsed. Error: ${err.message}`)
+            } else {
+                res.send(`Error while authorizing request with token ending in '${partialToken}': ${err}`)
+            }
+            
             return;
         }
 
