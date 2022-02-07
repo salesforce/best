82059409dfe3fefacdeda04e8fee58bc0f4c7935
Cătălin Mariș <cmaris@salesforce.com>
2022-02-07 15:05:53 -0800

    feat!: upgrade `puppeteer` to `v13.5.1`
    BREAKING CHANGE: Upgrade the headless Chrome version to `v100.0.4889.0`.
    
    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    
    Ref: https://github.com/puppeteer/puppeteer/releases/tag/v13.5.1
    
    Close: #297

--- packages/@best/runner-headless/src/headless.ts
+++ packages/@best/runner-headless/src/headless.ts
@@ -122,8 +122,8 @@ export default class HeadlessBrowser {
     static async getSpecs(): Promise<BrowserSpec[]> {
         // TODO: Create pupeteer test so we fail when upgrading
         return [
-            { name: 'chrome.headless', version: '97' },
-            { name: 'chrome', version: '97' }
+            { name: 'chrome.headless', version: '99' },
+            { name: 'chrome', version: '99' }
         ];
     }
 }
--- packages/best-benchmarks/best.config.js
+++ packages/best-benchmarks/best.config.js
@@ -8,7 +8,7 @@
 module.exports = {
     projectName: 'best-benchmark',
     metrics: ['script', 'aggregate', 'paint', 'layout'],
-    specs: { name: 'chrome.headless', version: 97 },
+    specs: { name: 'chrome.headless', version: 99 },
     runners: [
         {
             runner: "@best/runner-headless",
