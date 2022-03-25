066d7e19e195945b6852f2527d62e0f2c8c5d88c
Cătălin Mariș <cmaris@salesforce.com>
2022-03-24 18:55:40 -0700

    fix: chrome version number

--- packages/@best/runner-headless/src/headless.ts
+++ packages/@best/runner-headless/src/headless.ts
@@ -122,8 +122,8 @@ export default class HeadlessBrowser {
     static async getSpecs(): Promise<BrowserSpec[]> {
         // TODO: Create pupeteer test so we fail when upgrading
         return [
-            { name: 'chrome.headless', version: '99' },
-            { name: 'chrome', version: '99' }
+            { name: 'chrome.headless', version: '100' },
+            { name: 'chrome', version: '100' }
         ];
     }
 }
--- packages/lwc-example/best.config.js
+++ packages/lwc-example/best.config.js
@@ -13,7 +13,7 @@ module.exports = {
         }],
         ['rollup-plugin-replace', { 'process.env.NODE_ENV': JSON.stringify('production') }]
     ],
-    specs: { name: 'chrome.headless', version: 99 },
+    specs: { name: 'chrome.headless', version: 100 },
     runners: [
         {
             runner: "@best/runner-headless",
