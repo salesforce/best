e983da2896ff3f9bfe0a3e2ec341cfd5eb3169ff
sf-v <cmaris@salesforce.com>
2023-04-25 00:31:13 -0700

    fix: fetch `plotly` from own server instead of CDN

--- packages/@best/frontend/lwc-services.config.js
+++ packages/@best/frontend/lwc-services.config.js
@@ -8,6 +8,15 @@
 // Find the full example of all available configuration options at
 // https://github.com/muenzpraeger/create-lwc-app/blob/main/packages/lwc-services/example/lwc-services.config.js
 module.exports = {
-    resources: [{ from: 'src/resources/', to: 'dist/resources/' }],
     moduleDir: './src/modules',
+    resources: [
+        {
+            from: 'src/resources/',
+            to: 'dist/resources/',
+        },
+        {
+            from: '../../../node_modules/plotly.js-basic-dist-min/plotly-basic.min.js',
+            to: 'dist/plotly.min.js',
+        },
+    ],
 };
